a43734e08113f5819c5acc070fab7da892e8605d
Ardelia Anggun Saputri <ardeliaanggun4@gmail.com>
2026-09-07 10:03:21 +0700

    Mengubah Icon Daftar Kontak

--- Ardelia_BukuKontak/Ardelia_BukuKontak/lib/models/kontak.dart
+++ Ardelia_BukuKontak/Ardelia_BukuKontak/lib/models/kontak.dart
@@ -2,10 +2,12 @@ class Kontak {
   final String nama;
   final String email;
   final String noHp;
+  final String? kategori;   // <-- baru, nullable
 
   Kontak({
     required this.nama,
     required this.email,
     required this.noHp,
+    this.kategori,          // <-- tidak pakai 'required', jadi opsional
   });
 }
